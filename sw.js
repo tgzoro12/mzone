5743b95141c50f43c9fdca00782db4ae84adac4b
Pendrahub <jullermark1@gmail.com>
2026-01-20 23:23:27 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'mzone-v1';
+const urlsToCache = [
+  '/mzone/',
+  '/mzone/index.html',
+  '/mzone/dashboard.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
